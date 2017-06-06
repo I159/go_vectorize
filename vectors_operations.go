b8a326e1c9ec75e2b6dcd2481f39d9362a4d0a03
Ilya Pekelny <pekelny@gmail.com>
2017-06-06 23:26:18 +0300

    Static vectors operations

--- vectors_operations.go
+++ vectors_operations.go
@@ -5,8 +5,7 @@ import (
 	"sync"
 )
 
-// FIXME: operation
-func vector1DTo1D(output, inputA, inputB <-chan float64, wg sync.WaitGroup) {
+func vector1DTo1D(output, inputA, inputB <-chan float64, operation func(float64) float64, wg sync.WaitGroup) {
 	var a, b float64
 	for {
 		select {
@@ -20,7 +19,7 @@ func vector1DTo1D(output, inputA, inputB <-chan float64, wg sync.WaitGroup) {
 	}
 }
 
-func Vector1DTo1D(inputA, inputB chan float64, size int) (output chan float64) {
+func Vector1DTo1D(inputA, inputB <-chan float64, size int, operation func(float64, float64) float64) (output chan float64) {
 	var wg sync.WaitGroup
 	wg.Add(size)
 	output := make(chan float64, int(math.Log(size)))
@@ -33,7 +32,7 @@ func Vector1DTo1D(inputA, inputB chan float64, size int) (output chan float64) {
 	return
 }
 
-func vector2DTo2D(output, inputA, inputB <-chan <-chan float64, size int, wg sync.WaitGroup) {
+func vector2DTo2D(output, inputA, inputB <-chan <-chan float64, size int, operation func(float64, float64) float64, wg sync.WaitGroup) {
 	var a, b float64
 	for {
 		select {
@@ -42,16 +41,16 @@ func vector2DTo2D(output, inputA, inputB <-chan <-chan float64, size int, wg syn
 		default:
 			break
 		}
-		output <- Vector1DTo1D(a, b, shape[1])
+		output <- Vector1DTo1D(a, b, size)
 		wg.Done()
 	}
 }
 
-func Vector2DTo2D(inputA, inputB chan chan float64, shape [2]int) (output chan chan float64) {
+func Vector2DTo2D(inputA, inputB chan chan float64, shape [2]int, operation func(float64, float64) float64) (output chan chan float64) {
 	var wg sync.WaitGroup
 	wg.Add(shape[0])
 	output := make(chan chan float64, int(math.Log(shape[0])))
-	go vector2DTo2D(output, inputA, inputB, shape[1], wg)
+	go vector2DTo2D(output, inputA, inputB, shape[1], operation, wg)
 	go func() {
 		defer close(inputA)
 		defer close(inputB)
@@ -60,16 +59,70 @@ func Vector2DTo2D(inputA, inputB chan chan float64, shape [2]int) (output chan c
 	return
 }
 
-func vector1DStaticTo1D(inputA chan float64, inputB []float64) (output chan float64) {
-	output := make(chan float64, int(math.Log(len(inputB))))
+func vector1DStaticTo1D(output, inputA chan float64, inputB []float64, operation func(float64, float64) float64, wg sync.WaitGroup) {
 	for b := range inputB {
+	LOOP:
 		for {
 			select {
 			case a, ok := <-inputA:
 				if ok == true {
 					output <- operation(a, b)
+				} else {
+					break LOOP
 				}
+			default:
+				break LOOP
 			}
 		}
 	}
+}
+
+func Vector1DStaticTo1D(inputA chan float64, inputB []float64, operation func(float64, float64) float64) (output chan float64) {
+	var wg sync.WaitGroup
+	output = make(chan float64, math.Log(len(inputB)))
+	wg.Add(len(inputB))
+	go vector1DStaticTo1D(output, inputA, inputB, operation, wg)
+	go func() {
+		defer close(inputA)
+		wg.Wait()
+	}()
+	return
+}
+
+type LockingSlice struct {
+	sync.Mutex
+	Vector []float64
+}
+
+func mutate1D(inputA chan float64, inputB *LockingSlice, operation func(float64, float64) float64, wg sync.WaitGroup) {
+LOOP:
+	for i, v := range inputB {
+		for {
+			select {
+			case j, ok := <-inputA:
+				if ok == false {
+					break LOOP
+				} else {
+					inputB[i] = operation(j, v)
+				}
+			default:
+				break LOOP
+			}
+			wg.Done()
+		}
+	}
+
+}
+
+func Mutate1D(inputA chan float64, inputB *LockingSlice, operation func(float64, float64) float64) *LockingSlice {
+	var wg sync.WaitGroup
+	wg.Add(math.Log(len(inputB)))
+	inputB.Lock()
+	go mutate1D(inputA, inputB, operation, wg)
+	go func() {
+		defer close(inputA)
+		defer inputB.Unlock()
+		wg.Wait()
+	}()
+	return inputB
 }
